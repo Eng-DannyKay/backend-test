3c1e5243f1861110e5b78bd3070435a9bb011822
Emmanuel Mensah <102307671+Eng-DannyKay@users.noreply.github.com>
2025-09-28 15:03:09 +0000

    Update settings.py

--- eld/settings.py
+++ eld/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",  
 ]
 
 # Swagger Settings
@@ -166,7 +165,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
